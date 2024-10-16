cc2fbd018d177eeb0251c93624564adaa7156f32
Justin Richardson <justintrichardson@gmail.com>
2024-10-15 21:43:49 -0500

    new file:   code/dashboard.py 	new file:   code/data.py 	new file:   data/IN_covid19_20-23.csv 	new file:   data/IN_covid_report_county_historical.csv

---
+++ code/data.py
@@ -0,0 +1,17 @@
+# import pandas as pd
+
+# # Set your file path
+# in_st = r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv'
+# # Statewide dataset
+# in_st = pd.read_csv(in_st)
+
+# print(in_st.head())
+
+import pandas as pd
+
+# Set your file path using raw string
+in_st = r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv'
+
+# Statewide dataset
+in_st = pd.read_csv(in_st)
+print(in_st.head())
