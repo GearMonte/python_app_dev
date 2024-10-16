f70c9e612c8016979169a0155d325fbbf71af3bc
Justin Richardson <justintrichardson@gmail.com>
2024-10-15 22:38:07 -0500

    modified:   code/dashboard.py

--- code/dashboard.py
+++ code/dashboard.py
@@ -1,4 +1,16 @@
 import matplotlib.pyplot as plt
 from data import in_st, in_co
 
-print(in_st)
+# Step 1: Create your plots using matplotlib
+# Updating your color scheme:
+plt.rcParams['axes.prop_cycle'] = plt.cycler(
+    color=['#fbfec7', '#4bcdd7', '#f48217', '#2b3e4d', '#b4164e'])
+
+# First plot is the covid_deaths for the State
+fig1, ax1 = plt.subplots()
+ax1.bar(in_st.DATE, in_st.COVID_DEATHS)
+ax1.set_title('Indiana State: Covid 19 Deaths 2020-2023')
+ax1.setxlabel('Year')
+ax1.setylabel('Deaths')
+plt.show()
+
