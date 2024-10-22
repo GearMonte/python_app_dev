d85011541898319ec8a7b93d65d6a364217a6c8d
Justin Richardson <justintrichardson@gmail.com>
2024-10-21 21:55:04 -0500

    new file:   code/__pycache__/data.cpython-312.pyc 	modified:   code/dashboard.py 	modified:   code/data.py

--- code/dashboard.py
+++ code/dashboard.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from data import in_st, in_co
+from data import in_st #, in_co
 
 # Step 1: Create your plots using matplotlib
 # Updating your color scheme:
@@ -12,10 +12,10 @@ ax1.bar(in_st.DATE, in_st.COVID_DEATHS)
 ax1.set_title('Indiana State: Covid 19 Deaths 2020-2023')
 plt.show()
 
-# fig2, ax2 = plt.subplots()
-# ax2.barh(list(in_co.COUNTY_NAME), in_co.COVID_DEATHS)
-# ax2.set_title('Indiana County: Covid 19 Deaths by County')
-# plt.show()
+fig2, ax2 = plt.subplots()
+ax2.barh(list(in_co.COUNTY_NAME), in_co.COVID_DEATHS)
+ax2.set_title('Indiana County: Covid 19 Deaths by County')
+plt.show()
 
-# fig3, ax3 = plt.subplots()
-# ax3.pie(in_st.col)
+fig3, ax3 = plt.subplots()
+ax3.pie(in_st.col)
--- code/data.py
+++ code/data.py
@@ -5,23 +5,23 @@ from pyspark.sql.functions import col
 
 # Set your file path using raw string
 in_st = r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv'
-in_co = r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid_report_county_historical.csv'
+# in_co = r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid_report_county_historical.csv'
 
 # Statewide dataset
 in_st = pd.read_csv(in_st)
 # print(in_st.head())
 
 # County dataset
-in_co = pd.read_csv(in_co)
+# in_co = pd.read_csv(in_co)
 # print(in_co.head())
 
-spark = SparkSession.builder.appName("IN_State_Covid_data").getOrCreate()
-st_20 = spark.read.csv(r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv', header=True, inferSchma=True)
+# spark = SparkSession.builder.appName("IN_State_Covid_data").getOrCreate()
+# st_20 = spark.read.csv(r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv', header=True, inferSchma=True)
 
-# st_date = '2/26/2020'
-# end_date = '12/31/2020'
-st_date = '2/26/2020'
-end_date = '12/31/2020'
+# # st_date = '2\26\2020'
+# # end_date = '12\31\2020'
+# st_date = '2\26\2020'
+# end_date = '12\31\2020'
 
-st_20 = st_20.filter(col('DATE') >= st_date) & (col('DATE') <= end_date)
-st_20.show()
+# st_20 = st_20.filter(col('DATE') >= st_date) & (col('DATE') <= end_date)
+# # st_20.show()
