04999793ddcf3cd204e02e27aa2b72f9c1f90361
Justin Richardson <justintrichardson@gmail.com>
2024-10-29 21:58:15 -0500

    modified:   code/data.py

--- code/data.py
+++ code/data.py
@@ -13,14 +13,14 @@ in_st = pd.read_csv(in_st)
 
 # County dataset
 in_co = pd.read_csv(in_co)
-print(in_co.head())
+# print(in_co.head())
 
-# spark = SparkSession.builder.appName("IN_State_Covid_data").getOrCreate()
-# st_20 = spark.read.csv('C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv', header=True, inferSchma=True)
-# # st_date = '2\26\2020'
-# # end_date = '12\31\2020'
-# st_date = '2020-02-26'
-# end_date = '2020-12-31'
+spark = SparkSession.builder.appName("IN_State_Covid_data").getOrCreate()
+st_20 = spark.read.csv('C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv', header=True, inferSchma=True)
+# st_date = '2\26\2020'
+# end_date = '12\31\2020'
+st_date = '2020-02-26'
+end_date = '2020-12-31'
 
-# st_20 = st_20.filter(col('DATE') >= st_date) & (col('DATE') <= end_date)
-# # st_20.show()
+st_20 = st_20.filter(col('DATE') >= st_date) & (col('DATE') <= end_date)
+# st_20.show()
