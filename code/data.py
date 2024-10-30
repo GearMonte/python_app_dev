05c8a179d7afe4e971560192eeda94ea8c7587c3
Justin Richardson <justintrichardson@gmail.com>
2024-10-29 21:49:24 -0500

    modified:   code/__pycache__/data.cpython-312.pyc 	modified:   code/dashboard.py 	modified:   code/data.py

--- code/data.py
+++ code/data.py
@@ -16,12 +16,11 @@ in_co = pd.read_csv(in_co)
 print(in_co.head())
 
 # spark = SparkSession.builder.appName("IN_State_Covid_data").getOrCreate()
-# st_20 = spark.read.csv(r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv', header=True, inferSchma=True)
-
+# st_20 = spark.read.csv('C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv', header=True, inferSchma=True)
 # # st_date = '2\26\2020'
 # # end_date = '12\31\2020'
-# st_date = '2\26\2020'
-# end_date = '12\31\2020'
+# st_date = '2020-02-26'
+# end_date = '2020-12-31'
 
 # st_20 = st_20.filter(col('DATE') >= st_date) & (col('DATE') <= end_date)
 # # st_20.show()
