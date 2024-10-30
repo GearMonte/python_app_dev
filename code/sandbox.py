bd4dd50852f86b19d60a47c2ce4917eae1e95cad
Justin Richardson <justintrichardson@gmail.com>
2024-10-29 22:05:46 -0500

    new file:   code/sandbox.py

---
+++ code/sandbox.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+in_st = r'C:\Users\Justin\OneDrive\Desktop\Python\python_app_dev\data\IN_covid19_20-23.csv'
+
+# Statewide dataset
+in_st = pd.read_csv(in_st)
+print(in_st.head())
